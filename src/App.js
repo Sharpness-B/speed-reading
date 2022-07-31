082173c28b85c1b7414c7a654cbe7770b9448443
Bendik Skarpnes <Sharpness-B@users.noreply.github.com>
2022-07-31 16:37:48 +0200

    add building blocks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
+import React, { useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+import Metronome from '@kevinorriss/react-metronome'
+import { Vex } from "vexflow";
+
+const { Factory } = Vex.Flow;
+
 function App() {
+  const draw = () => {
+    // clear
+    document.getElementById('output').innerHTML = ""
+
+    // draw
+    const vf = new Factory({
+      renderer: { elementId: 'output', width: 600, height: 150 },
+    });
+
+    const score = vf.EasyScore();
+    const system = vf.System();
+
+    system
+      .addStave({
+        voices: [
+          score.voice(
+            score.notes('')
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+              .concat(score.beam(score.notes('A4/16, E4, C4, D4')))
+            , { time: '8/4' })
+        ],
+      })
+      .addClef('treble')
+      .addTimeSignature('8/4');
+
+    vf.draw();
+  }
+
+
+  
+  useEffect(() => {
+    draw()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>How fast can you successfully play all notes on first try?</p>
+        <Metronome />
+        <div id="output"></div>
+        <button onClick={draw}>Ny</button>
       </header>
     </div>
   );
